f1f144a343d83d23d1c3b81d02d35e4489fe9efa
Gabriel Neres <gabrielneres-12@hotmail.com>
2019-10-23 19:20:28 -0300

    UPDATE - Implementação da importação dos dados de deputados e suas redes sociais, criação dos TOs.

---
+++ app/To/RedeSocialDeputadoTO.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\To;
+
+
+class DeputadoTO
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var
+     */
+    private $ds_nome;
+
+    /**
+     * @var
+     */
+    private $co_deputado;
+
+    /**
+     * @var
+     */
+    private $ds_partido;
+
+    /**
+     * @var
+     */
+    private $ds_deputado;
+
+    /**
+     * @var
+     */
+    private $created_at;
+
+    /**
+     * @var
+     */
+    private $updated_at;
+
+    /**
+     * @var array
+     */
+    private $redesSociaisDeputados;
+
+}
